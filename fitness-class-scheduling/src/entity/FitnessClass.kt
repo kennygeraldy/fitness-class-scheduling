85619380af35eed9b9754ba0faed15eb77826519
tasyaraishaniaa <tasyaraishania@gmail.com>
2024-09-30 16:04:32 +0700

    scheduleFitnessClasses, isValidClassType, isClassOverlapping

--- fitness-class-scheduling/src/entity/FitnessClass.kt
+++ fitness-class-scheduling/src/entity/FitnessClass.kt
@@ -19,5 +19,6 @@ class FitnessClass(
         if (name.isBlank()) throw IllegalArgumentException("Name cannot be blank")
         if (instructor.isBlank()) throw IllegalArgumentException("Instructor cannot be blank")
         require(duration > 0 && duration <= 60) { "Duration must be between 1 and 60 minutes." }
+        require(maxParticipants > 0) { "Max participants must be greater than 0." }
     }
 }
