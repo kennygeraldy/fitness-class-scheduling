c0daa3f2ff8f899a80679efffb05fd411d776c2f
Bryan Felix <bryanfelix2001@gmail.com>
2024-09-30 14:52:53 +0700

    init fcm

--- fitness-class-scheduling/src/services/FitnessCenterManager.kt
+++ fitness-class-scheduling/src/services/FitnessCenterManager.kt
@@ -1,1 +1,36 @@
 package services
+
+import entity.FitnessClass
+import entity.Member
+import enum.MembershipType
+import java.time.LocalDate
+
+class FitnessCenterManager {
+
+    val fitnessClasss = mutableListOf<FitnessClass>()
+    val members = mutableListOf<Member>()
+
+    fun registerMember(name:String, membershipType: String, subscriptionStartDate:String){
+
+    }
+
+    fun scheduleFitnessClasses(name:String, instructor:String, date:String, time:String, duration:Int, maxParticipants:Int){
+
+    }
+
+    fun enrollInClass(memberId:Int, fitnessClassId:List<Int>){
+
+    }
+
+    fun classAttendanceTracking(memberId: Int){
+
+    }
+
+    fun membershipRenewal(memberId: Int){
+
+    }
+
+    fun listClassesByDate(date:String, time:String){
+
+    }
+}
