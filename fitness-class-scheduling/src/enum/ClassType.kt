9f5469952432bdd1e0d108410eaef19f45a8f444
Bryan Felix <bryanfelix2001@gmail.com>
2024-09-30 19:59:13 +0700

    seperate enum class

---
+++ fitness-class-scheduling/src/enum/ClassType.kt
@@ -0,0 +1,5 @@
+package enum
+
+enum class ClassType {
+    Regular, Special, Exclusive
+}
--- fitness-class-scheduling/src/enum/MembershipType.kt
+++ fitness-class-scheduling/src/enum/MembershipType.kt
@@ -3,7 +3,3 @@ package enum
 enum class MembershipType {
     Basic, Premium, VIP
 }
-
-enum class ClassType {
-    Regular, Special, Exclusive
-}
