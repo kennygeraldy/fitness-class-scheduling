5b6a8c715a025c4bb5db96162e2d089af136b948
tasyaraishaniaa <tasyaraishania@gmail.com>
2024-09-30 14:09:56 +0700

    FitnessClass

--- fitness-class-scheduling/src/entity/FitnessClass.kt
+++ fitness-class-scheduling/src/entity/FitnessClass.kt
@@ -1,1 +1,20 @@
 package entity
+
+import java.time.LocalDate
+import java.time.LocalTime
+
+class FitnessClass(
+    var id: Int,
+    var name: String,
+    var instructor: String,
+    var date: LocalDate,
+    var time: LocalTime,
+    var duration: Int,
+    var maxParticipants: Int,
+    var enrolledMembers: MutableList<Member> = mutableListOf()
+) {
+    init {
+        if (name.isBlank()) throw IllegalArgumentException("Name cannot be blank")
+        if (instructor.isBlank()) throw IllegalArgumentException("Instructor cannot be blank")
+    }
+}
